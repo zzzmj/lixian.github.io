44e1b08ec384387bdaa2c641899ad9afa4f343a8
zmj <18270632198@163.com>
2019-02-27 13:24:05 +0800

    Site updated: 2019-02-27 13:24:04

---
+++ demo/three-column-layout/js/flying.js
@@ -0,0 +1,34 @@
+const $ = (selector) => {
+    return document.querySelector(selector)
+}
+
+const leftBox = $('.left')
+const midBox = $('.main')
+const rightBox = $('.right')
+
+const insertTpl = (element) => {
+    const p = `
+        <p>插入一些新内容</p>
+    `
+    element.insertAdjacentHTML('beforeend', p)
+}
+
+const handleClick = () => {
+    $('.btn-left').addEventListener('click', () => {
+        insertTpl(leftBox)
+    })
+    
+    $('.btn-right').addEventListener('click', () => {
+        insertTpl(rightBox)
+    })
+
+    $('.btn-middle').addEventListener('click', () => {
+        insertTpl(midBox)
+    })
+}
+
+const __main = () => {
+    handleClick()
+}
+
+__main()
