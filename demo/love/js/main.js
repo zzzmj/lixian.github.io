856e697e3564583cdfbb10ac142fb7b126ef4d4a
zmj <18270632198@163.com>
2019-08-15 18:00:22 +0800

    Site updated: 2019-08-15 18:00:20

--- demo/love/js/main.js
+++ demo/love/js/main.js
@@ -1,27 +1,34 @@
 // 结束时间
 var countDown = function() {
     var endTime = new Date("07/28/2019 12:00:00")
-    var diff = endTime.getTime() - Date.now()   
-    var _second = 1000,
+    var diff = endTime.getTime() - Date.now()
+    if (diff > 0) {
+        var _second = 1000,
         _minute = _second * 60,
         _hour = _minute * 60,
         _day = _hour * 24
 
-    var days = Math.floor(diff / _day)
-    var hours = Math.floor((diff % _day) / _hour)
-    var minutes = Math.floor((diff % _hour) / _minute)
-    var seconds = Math.floor((diff % _minute) / _second)
+        var days = Math.floor(diff / _day)
+        var hours = Math.floor((diff % _day) / _hour)
+        var minutes = Math.floor((diff % _hour) / _minute)
+        var seconds = Math.floor((diff % _minute) / _second)
 
-    if (days < 10) days = '00' + days
-    else if (days < 100) days = '0' + days
-    if (seconds < 10) seconds = '0' + seconds
-    if (minutes < 10) minutes = '0' + minutes
-    if (hours < 10) hours = '0' + hours
-    $('.countdown .days').innerText = days
-    $('.countdown .hours').innerText = hours
-    $('.countdown .minutes').innerText = minutes
-    $('.countdown .seconds').innerText = seconds
-    console.log(`${days} ${hours} ${minutes} ${seconds}`);
+        if (days < 10) days = '00' + days
+        else if (days < 100) days = '0' + days
+        if (seconds < 10) seconds = '0' + seconds
+        if (minutes < 10) minutes = '0' + minutes
+        if (hours < 10) hours = '0' + hours
+        $('.countdown .days').innerText = days
+        $('.countdown .hours').innerText = hours
+        $('.countdown .minutes').innerText = minutes
+        $('.countdown .seconds').innerText = seconds
+        $('.flag').classList.add('active')
+        $('.countdown').classList.remove('active')
+    } else {
+        var list = $('.countdown .seperator')
+        $('.flag').classList.remove('active')
+        $('.countdown').classList.add('active')
+    }   
 }
 
 
@@ -47,7 +54,6 @@ var countUp = function() {
     $('.countup .hours').innerText = hours
     $('.countup .minutes').innerText = minutes
     $('.countup .seconds').innerText = seconds
-    console.log(`${days} ${hours} ${minutes} ${seconds}`);
 }
 countDown()
 countUp()
